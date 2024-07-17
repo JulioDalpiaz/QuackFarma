3f74368c9207c68b246efef82a576f39fbf4a9b1
viniciusb <vinidsb581@gmail.com>
2024-07-17 18:23:39 -0400

    Fixed the nodemon problem, now it works properly; addes a separate file for db connection; added cors for cross url communication, so we can respond to axios requests fromfront end and updated readme file, now docker command are simpler and not ambiguous.

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
-Clone este reposittório para sua pasta de trabalho
-Aba o terminal na pasta e rode um dos seguintes
-    docker compose up
-    OU
-    docker-compose up --build
-No windows talvez precise usar o com --build
-E no linux, dependendo da configuração, coloque o sudo antes :) 
-    
-O docker ira dar pull nas imagens e iniciar 3 containers
-servidor, postgres e pgadmin
-Espere todos os 3 estaram prontos, observe se entre tudo que aparece no terminal apareceu:
-    db-1       | 2024-07-14 18:19:29.883 UTC [1] LOG:  database system is ready to accept connections
-    web-1      | Servidor rodando em http://0.0.0.0:3000
-    pgadmin-1  | [2024-07-14 18:20:20 +0000] [117] [INFO] Booting worker with pid:
+Clone este repositório para sua pasta de trabalho;<br>
+Abra o terminal na pasta e rode: <br>
+    docker compose up --build<br>
+na primeira ve, nas proximas apenas um<br>
+    docker compose up<br>
 
-O pgadmin deve demorar mais e ser o ultimo a aparecer, e depois disso:
-Para acessar a página de testes va para http://localhost:3001
-Há 4 rotas para teste (ou 5 se contar a '/', que mostra os formulários)
-http://localhost:3001/insert
-http://localhost:3001/select
-http://localhost:3001/update
+E no linux, dependendo da configuração, coloque o sudo antes :smile:
+    
+O docker irá dar pull nas imagens e iniciar 2 containers:<br>
+servidor e postgres.<br>
+Espere todos os 2 estarem prontos, observe no no terminal se as seguintes mensagens aparecem:<br>
+    db-1       | 2024-07-14 18:19:29.883 UTC [1] LOG:  database system is ready to accept connection<br>
+    web-1      | Server running at http://0.0.0.0:3000<br>
+
+Após isso: <br>
+Vá ao navegador e acesse http://localhost:3001. <br>
+Há 4 rotas para teste (ou 5 se contar a '/', que mostra os formulários):<br>
+http://localhost:3001/insert<br>
+http://localhost:3001/select<br> 
+http://localhost:3001/update<br>
 http://localhost:3001/delete
--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 //basic
 require('dotenv').config();
 const path = require('path');
+const cors = require("cors");
 
 //express config
 const express = require('express');
 const app = express();
 const HOST = '0.0.0.0';
-const PORT = 3000;
-app.use(express.static(path.join(__dirname, 'public')));
+const PORT = 3000; //container port
+
+app.use(cors()); //for axios cross url access, so we can respond to fornt end requests
+app.use(express.static(path.join(__dirname, 'public'))); //just for some route tests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//db config, connect to postgres
-const { Pool } = require('pg');
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL
-});
+//database, pg pool
+const dbPool = require('./dbConnection');
 
 //basic test routes
 app.get('/', async (req, res) => {
     try {
-        await pool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, nome VARCHAR(100), idade INT)');
+        await dbPool.query('CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, nome VARCHAR(100), idade INT)');
         res.sendFile(path.join(__dirname, 'public', 'teste.html'));
     } catch (error) {
         console.error('Erro na rota /:', error.message);
-        res.status(500).send('Erro ao criar tabela. Veirfique o log');
+        res.status(500).send('Erro ao criar tabela. Veirfique o log.');
     }
 });
 
 app.post('/insert', async (req, res) => {
     const { nome, idade } = req.body;
     try {
-        await pool.query('INSERT INTO users (nome, idade) VALUES ($1, $2)', [nome, idade]);
+        await dbPool.query('INSERT INTO users (nome, idade) VALUES ($1, $2)', [nome, idade]);
         res.send('Usuário criado com sucesso!');
     } catch (error) {
         console.error('Erro na rota /insert:', error.message);
@@ -41,7 +41,7 @@ app.post('/insert', async (req, res) => {
 
 app.get('/select', async (req, res) => {
     try {
-        const resposta = await pool.query('SELECT * FROM users');
+        const resposta = await dbPool.query('SELECT * FROM users');
         res.json(resposta.rows); 
     } catch (error) {
         console.error('Erro na rota /select:', error.message);
@@ -52,7 +52,7 @@ app.get('/select', async (req, res) => {
 app.post('/update', async (req, res) => {
     const { nome, novoNome, idade } = req.body;
     try {
-        await pool.query('UPDATE users SET nome = $1, idade = $2 WHERE nome = $3', [novoNome, idade, nome]);
+        await dbPool.query('UPDATE users SET nome = $1, idade = $2 WHERE nome = $3', [novoNome, idade, nome]);
         res.send('Usuário atualizado com sucesso!');
     } catch (error) {
         console.error('Erro na rota /update:', error.message);
@@ -64,7 +64,7 @@ app.post('/update', async (req, res) => {
 app.post('/delete', async (req, res) => {
     const { nome } = req.body;
     try {
-        await pool.query('DELETE FROM users WHERE nome = $1', [nome]);
+        await dbPool.query('DELETE FROM users WHERE nome = $1', [nome]);
         res.send('Usuário deletado com sucesso!');
     } catch (error) {
         console.error('Erro na rota /delete:', error.message);
@@ -74,5 +74,5 @@ app.post('/delete', async (req, res) => {
 
 //ouvirodira
 app.listen(PORT, HOST, () => {
-    console.log(`Ta ligado ta na mega, sintonize http://${HOST}:${PORT}`);
+    console.log(`Server running at http://${HOST}:${PORT}`);
 });
